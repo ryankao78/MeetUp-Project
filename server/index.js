239b5f8b7d73db604385eae73185f11f9a7abdd7
Jen Tse Kao <36420454+ryankao78@users.noreply.github.com>
2022-04-20 21:33:51 -0500

    add google sign-in
    add google sign-in

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express(); // initialize express 
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes); // this line has to be below app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://root:root@cluster0.0upqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
+// https://stackoverflow.com/questions/69030963/error-usefindandmodify-is-an-invalid-option
+
